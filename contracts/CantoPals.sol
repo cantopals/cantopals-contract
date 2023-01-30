d6a3dd8a3f8bd43db9fbb1120ae9f3de40547537
Ismail Kattakath <ikattakath@homewoodhealth.com>
2023-01-30 10:00:24 -0500

    Ready to deploy

--- contracts/CantoPals.sol
+++ contracts/CantoPals.sol
@@ -3,40 +3,84 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CantoPals is ERC721Enumerable, ERC721URIStorage, Ownable {
+contract CantoPals is ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
+    using Counters for Counters.Counter;
 
-    uint256 public maxSupply;
-    uint256 public userLimit;
+    uint256 public maxMint;
+    uint256 public maxClaim;
+    Counters.Counter private mintCount;
+    Counters.Counter private claimCount;
+    mapping(address => bool) private whitelistedWallets;
     string private baseUrl;
     string private unrevealedUrl;
     bool private isRevealed;
-    mapping(address => uint) public walletMints;
+    uint256 public cost = 25 ether;
 
-    constructor(uint256 _maxSupply, uint256 _userLimit, string memory _collectionName, string memory _collectionSymbol, string memory _baseUrl, string memory _unrevealedUrl) ERC721(_collectionName, _collectionSymbol) {
-            maxSupply = _maxSupply;
-            userLimit = _userLimit;
-            baseUrl = _baseUrl;
-            unrevealedUrl = _unrevealedUrl;
-            isRevealed = false;
-        }
-
-    function safeMint(uint256 _mintAmount) public {
-        uint256 _supply = totalSupply();
-        require(_supply <= maxSupply, "Sold out!");
-        require(walletMints[msg.sender] + _mintAmount <= userLimit, string.concat("You can only mint ", Strings.toString(userLimit), " NFTs"));
-        for (uint256 i = 1; i <= _mintAmount; i++) {
-            uint256 _tokenId = _supply + i;
-            _safeMint(msg.sender, _tokenId);
-            _setTokenURI(_tokenId, string.concat(baseUrl, Strings.toString(_tokenId), ".json"));
-        }
-        walletMints[msg.sender] = walletMints[msg.sender] + _mintAmount;
+    constructor(string memory _baseUrl, string memory _unrevealedUrl) ERC721('CantoPals', 'CPALS') {
+        maxMint = 2667;
+        maxClaim = 666;
+        baseUrl = _baseUrl;
+        unrevealedUrl = _unrevealedUrl;
+        isRevealed = false;
     }
 
-    // The following functions are overrides required by Solidity.
+    // Write an owner mint function
 
+    function mint(uint256 _mintAmount) public whenNotPaused payable {
+        require(maxMint >= mintCount.current() + _mintAmount, string.concat("Only ", Strings.toString(maxMint - mintCount.current()), " left to mint!"));
+        require(msg.value >= cost * _mintAmount);
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            uint256 tokenId = totalSupply() + 1;
+            _safeMint(msg.sender, tokenId);
+            _setTokenURI(tokenId, string.concat(baseUrl, Strings.toString(tokenId), ".json"));
+            mintCount.increment();
+        }
+    }
+
+    function claim() public whenNotPaused {
+        require(whitelistedWallets[msg.sender], "You are not eligible to claim this NFT. Please try minting!");
+        require(maxClaim >= claimCount.current() + 1, "No NFT left to claim!");
+        uint256 tokenId = totalSupply() + 1;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, string.concat(baseUrl, Strings.toString(tokenId), ".json"));
+        claimCount.increment();
+        whitelistedWallets[msg.sender] = false;
+    }
+
+    /* Only owner can execute these functions. */
+    function revealCollection() public onlyOwner {
+        isRevealed = true;
+    }
+
+    function populateWhitelist(address[] calldata _wallets) public onlyOwner {
+        for(uint256 i; i < _wallets.length; i++) {
+            whitelistedWallets[_wallets[i]] = true;
+        }
+    }
+
+    function setCost(uint256 _cost) public onlyOwner {
+        cost = _cost * (1 ether);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
+    }
+
+    /* The following functions are overrides required by Solidity. */
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable)
     {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
@@ -59,11 +103,5 @@ contract CantoPals is ERC721Enumerable, ERC721URIStorage, Ownable {
         } else {
             return unrevealedUrl;
         }
-    }
-
-    // Only owner
-
-    function revealCollection() public onlyOwner {
-        isRevealed = true;
     }
 }
